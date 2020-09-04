9a0f6267409f040f5bf8de4f75422d8eada02883
clearine <32025159+clearine@users.noreply.github.com>
2020-09-04 12:33:01 -0230

    Add fetchWeatherFromIP()

--- lib/src/weather.dart
+++ lib/src/weather.dart
@@ -3,11 +3,26 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class Weather {
-  Weather({this.cityName, this.cityId, this.ZipCode});
+  Weather({this.cityName, this.cityId, this.zipCode});
 
   String cityName;
   int cityId;
-  int ZipCode;
+  int zipCode;
+
+  Future<String> fetchWeatherFromIP() async {
+    var ipUrl = await get('http://ip-api.com/json/');
+    var ipJson = jsonDecode(ipUrl.body);
+    var weatherUrl =
+        'https://api.openweathermap.org/data/2.5/weather?q=${ipJson['city']}&units=metric&appid=KEY';
+    var weatherRequest = await get(weatherUrl);
+    var weatherJson = jsonDecode(weatherRequest.body);
+
+    return '''
+    Description: ${weatherJson['weather'][0]['description']}
+    Temperature (C): ${weatherJson['main']['temp']}
+    Windspeed (KM): ${weatherJson['wind']['speed']}
+    ''';
+  }
 
   Future<String> fetchWeatherFromName() async {
     if (cityName == null) return 'No city name provided.';
