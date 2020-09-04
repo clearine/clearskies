a20fdae1bb251bdca88275bf9144440e84dd9eff
clearine <32025159+clearine@users.noreply.github.com>
2020-09-03 22:38:06 -0230

    Added weather class and ability to fetch weather from city name

---
+++ lib/src/weather.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class Weather {
+  String cityName;
+  int cityId;
+  int ZipCode;
+
+  Future<String> fetchWeatherFromName() async {
+    if (cityName == null) return 'No name provided.';
+
+    var url =
+        'https://api.openweathermap.org/data/2.5/weather?q=${cityName.split(' ').join('+')}&units=metric&appid=KEY';
+    var request = await get(url);
+    var json = jsonDecode(request.body);
+
+    return '''
+    Description: ${json['weather'][0]['description']}
+    Temperature (C): ${json['main']['temp']}
+    Windspeed (KM): ${json['wind']['speed']}
+    ''';
+  }
+}
