06be4ecd0205d23dcb2d74c879a6a70e1e47399d
clearine <32025159+clearine@users.noreply.github.com>
2020-09-05 20:18:19 -0230

    Added some basic error handling

--- bin/clearskies.dart
+++ bin/clearskies.dart
@@ -3,23 +3,42 @@ import 'package:clearskies/src/weather.dart';
 
 final parser = ArgParser(allowTrailingOptions: false);
 
+// TODO: Add some more error handling.
+
 void main(List<String> arguments) async {
-  parser.addOption('mode', abbr: 'm', allowed: ['name', 'id', 'zip']);
+  parser.addOption('mode', abbr: 'm', allowed: ['name', 'id', 'ip', 'zip']);
   var results = parser.parse(arguments);
 
   if (results['mode'] == 'name') {
-    var weather =
-        await Weather(cityName: results.rest[0]).fetchWeatherFromName();
-    print(weather);
+    try {
+      var weather =
+          await Weather(cityName: results.rest[0]).fetchWeatherFromName();
+      print(weather);
+    } on NoSuchMethodError {
+      print('Not a valid city.');
+    }
   } else if (results['mode'] == 'id') {
-    var id = int.parse(results.rest[0]);
-    var weather = await Weather(cityId: id).fetchWeatherFromID();
-    print(weather);
+    try {
+      var id = int.parse(results.rest[0]);
+      var weather = await Weather(cityId: id).fetchWeatherFromID();
+      print(weather);
+    } on NoSuchMethodError {
+      print('Not a valid ID');
+    }
+  } else if (results['mode'] == 'ip') {
+    try {
+      var weather = await Weather().fetchWeatherFromIP();
+      print(weather);
+    } on NoSuchMethodError {
+      print('Unable to fetch weather (Did you put in your API key?)');
+    }
   } else if (results['mode'] == 'zip') {
-    var zip = int.parse(results.rest[0]);
-    var weather = await Weather(zipCode: zip).fetchWeatherFromZIP();
-    print(weather);
-  } else {
-    print('${results.rest[0]} is not a valid option.');
+    try {
+      var zip = int.parse(results.rest[0]);
+      var weather = await Weather(zipCode: zip).fetchWeatherFromZIP();
+      print(weather);
+    } on NoSuchMethodError {
+      print('Not a valid ZIP code.');
+    }
   }
 }
