bec521d7e7bb3809a895753b78ee89aeae774df9
clearine <32025159+clearine@users.noreply.github.com>
2020-10-31 02:51:15 -0230

    Implement user input using new library

--- bin/clearskies.dart
+++ bin/clearskies.dart
@@ -1,5 +1,35 @@
 import 'package:clearskies/src/user_interface.dart';
+import 'package:dart_console/dart_console.dart';
 
+final interface = UserInterface();
+final console = Console();
 void main() {
-  var window = UserInterface();
+  final row = (console.windowHeight / 2).round() - 1;
+
+  console
+    ..clearScreen()
+    ..hideCursor()
+    ..setBackgroundColor(ConsoleColor.white)
+    ..setForegroundColor(ConsoleColor.black)
+    ..writeLine('Clear Skies', TextAlignment.center)
+    ..resetColorAttributes()
+    ..cursorPosition = Coordinate(row - 2, 0)
+    ..writeLine('test text', TextAlignment.center);
+
+  var key = console.readKey();
+
+  switch (key.char) {
+    case 'd':
+      interface.showWeatherFromID();
+      break;
+    case 'i':
+      interface.showWeatherFromIP();
+      break;
+    case 'n':
+      interface.showWeatherFromName();
+      break;
+    case 'z':
+      interface.showWeatherFromZIP();
+      break;
+  }
 }
--- lib/src/user_interface.dart
+++ lib/src/user_interface.dart
@@ -4,18 +4,37 @@ import 'package:dart_console/dart_console.dart';
 final console = Console();
 
 class UserInterface {
-  var screens = <Function>[
-    (() {
-      final row = (console.windowHeight / 2).round() - 1;
+  void showWeatherFromID() async {
+    console.clearScreen();
+    console.showCursor();
+    console.writeLine('Enter a city ID: ');
+    final input = console.readLine(cancelOnBreak: true);
+    final cityId = int.parse(input);
+    console
+        .writeLine(await Weather('key', cityId: cityId).fetchWeatherFromID());
+  }
 
-      console
-        ..clearScreen()
-        ..setBackgroundColor(ConsoleColor.white)
-        ..setForegroundColor(ConsoleColor.black)
-        ..writeLine('Clear Skies', TextAlignment.center)
-        ..resetColorAttributes()
-        ..cursorPosition = Coordinate(row - 2, 0)
-        ..writeLine('test', TextAlignment.center);
-    })
-  ];
+  void showWeatherFromIP() async {
+    console.clearScreen();
+    console.writeLine(await Weather('key').fetchWeatherFromIP());
+  }
+
+  void showWeatherFromName() async {
+    console.clearScreen();
+    console.showCursor();
+    console.writeLine('Enter a city name: ');
+    final name = console.readLine(cancelOnBreak: true);
+    console
+        .writeLine(await Weather('key', cityName: name).fetchWeatherFromID());
+  }
+
+  void showWeatherFromZIP() async {
+    console.clearScreen();
+    console.showCursor();
+    console.writeLine('Enter a ZIP code: ');
+    final input = console.readLine(cancelOnBreak: true);
+    final zipCode = int.parse(input);
+    console.writeLine(
+        await Weather('key', zipCode: zipCode).fetchWeatherFromZIP());
+  }
 }
