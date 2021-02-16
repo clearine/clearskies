c232329face8209d627270582c8ff6e66535eddc
clearine <32025159+clearine@users.noreply.github.com>
2021-02-15 23:47:19 -0330

    Some fixes to help memory usage

--- lib/src/weather.dart
+++ lib/src/weather.dart
@@ -9,30 +9,32 @@ class Weather {
   Weather(this.apiKey);
   String apiKey;
 
-  final Map<dynamic, String> errorCodes = const {
+  final Map<dynamic, String> _errorCodes = const {
     401: 'Error 401. Did you provide an API key?',
     '404': 'Error 404. Make sure the provided name/ID/ZIP is valid.',
     429: 'Error 429. You have made more than 60 API calls per minute.'
   };
 
-  bool checkForError(code) {
-    if (errorCodes[code] == null) return false;
+  bool _checkForError(code) {
+    if (_errorCodes[code] == null) return false;
     return true;
+  }
+
+  dynamic _getJson(String url) async {
+    var getRequest = await get(url);
+    return jsonDecode(getRequest.body);
   }
 
   // TODO: Make output fancier.
   // TODO: Fix output formatting.
 
   Future<String> fetchWeatherFromIP() async {
-    var ipUrl = await get('http://ip-api.com/json/');
-    var ipJson = jsonDecode(ipUrl.body);
-    var weatherUrl =
-        'https://api.openweathermap.org/data/2.5/weather?q=${ipJson['city']}&units=metric&appid=$apiKey';
-    var weatherRequest = await get(weatherUrl);
-    var weatherJson = jsonDecode(weatherRequest.body);
+    var ipJson = await _getJson('http://ip-api.com/json/');
+    var weatherJson = await _getJson(
+        'https://api.openweathermap.org/data/2.5/weather?q=${ipJson['city']}&units=metric&appid=$apiKey');
 
-    if (checkForError(weatherJson['cod'])) {
-      return errorCodes[weatherJson['cod']];
+    if (_checkForError(weatherJson['cod'])) {
+      return _errorCodes[weatherJson['cod']];
     }
 
     return '''
@@ -45,13 +47,11 @@ class Weather {
   Future<String> fetchWeatherFromName(String name) async {
     if (name == null) return 'No city name provided.';
 
-    var url =
-        'https://api.openweathermap.org/data/2.5/weather?q=${name.split(' ').join('+')}&units=metric&appid=$apiKey';
-    var request = await get(url);
-    var json = jsonDecode(request.body);
+    var json = await _getJson(
+        'https://api.openweathermap.org/data/2.5/weather?q=${name.split(' ').join('+')}&units=metric&appid=$apiKey');
 
-    if (checkForError(json['cod'])) {
-      return errorCodes[json['cod']];
+    if (_checkForError(json['cod'])) {
+      return _errorCodes[json['cod']];
     }
 
     return '''
@@ -64,13 +64,11 @@ class Weather {
   Future<String> fetchWeatherFromID(int id) async {
     if (id == null) return 'No city ID provided.';
 
-    var url =
-        'https://api.openweathermap.org/data/2.5/weather?id=${id}&units=metric&appid=$apiKey';
-    var request = await get(url);
-    var json = jsonDecode(request.body);
+    var json = await _getJson(
+        'https://api.openweathermap.org/data/2.5/weather?id=${id}&units=metric&appid=$apiKey');
 
-    if (checkForError(json['cod'])) {
-      return errorCodes[json['cod']];
+    if (_checkForError(json['cod'])) {
+      return _errorCodes[json['cod']];
     }
 
     return '''
@@ -83,13 +81,11 @@ class Weather {
   Future<String> fetchWeatherFromZIP(int zip) async {
     if (zip == null) return 'No ZIP code provided.';
 
-    var url =
-        'https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=metric&appid=$apiKey';
-    var request = await get(url);
-    var json = jsonDecode(request.body);
+    var json = await _getJson(
+        'https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=metric&appid=$apiKey');
 
-    if (checkForError(json['cod'])) {
-      return errorCodes[json['cod']];
+    if (_checkForError(json['cod'])) {
+      return _errorCodes[json['cod']];
     }
 
     return '''
